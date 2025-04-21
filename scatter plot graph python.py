5e060eea0a28dd7ec36beca5f433f727ce262541
khan123456110 <murshid1257@gmail.com>
2025-04-21 18:32:32 +0500

    Add files via upload

---
+++ scatter plot graph python.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Apr 21 17:21:05 2025
+
+@author: test
+"""
+
+import matplotlib.pyplot as plt
+x=[1,2,3,4,5]
+y=[5,4,3,2,1]
+plt.scatter(x,y)
+plt.xlabel('X-axis')
+plt.ylabel('Y-axis')
+plt.title('scatter plot')
+plt.show()               
