a8ff04ace6fe59f4768feaf46a87787c72048eac
khan123456110 <murshid1257@gmail.com>
2025-04-21 17:50:24 +0500

    Add files via upload

---
+++ Simple line graph python.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Apr 21 17:21:05 2025
+
+@author: test
+"""
+
+import matplotlib.pyplot as plt
+x = [1,2,3,4,5]
+y = [1,2,8,9,10]
+plt.plot(x,y)
+plt.title('my first graph')
+plt.xlabel('X-axis')
+plt.ylabel('Y-axis')
+plt.show()               
